bc45a50c31f36a40d784dca6eda5c0bee9300747
Ethan Wojcinski <ethantanner@me.com>
2015-09-05 13:36:59 -0500

    initial commit

---
+++ app/src/main/java/org/feelthebern/android/api/models/Page.java
@@ -0,0 +1,17 @@
+package org.feelthebern.android.api.models;
+
+/**
+ * Created by AndrewOrobator on 9/4/15.
+ */
+public class Page implements ApiItem {
+    private String title;
+    private String url;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}

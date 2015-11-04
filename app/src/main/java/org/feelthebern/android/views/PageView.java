4d9e29f47e2baaed2acac5b1b7abff2f899e4707
patrickcousins <pj@pj-co.com>
2015-11-04 02:44:22 -0500

    Set LayoutManager BERFORE calling takeView()

--- app/src/main/java/org/feelthebern/android/views/PageView.java
+++ app/src/main/java/org/feelthebern/android/views/PageView.java
@@ -58,8 +58,8 @@ public class PageView extends RecyclerView {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        presenter.takeView(this);
         setLayoutManager(getContext());
+        presenter.takeView(this);
     }
 
     @Override

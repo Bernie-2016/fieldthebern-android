4f3682c25e2f63b6b20aa2f9e5a4cf4baabfcdd0
patrickcousins <pj@pj-co.com>
2015-11-06 18:15:02 -0500

    Explain all the things

--- app/src/main/java/com/berniesanders/canvass/screens/TemplateScreen.java
+++ app/src/main/java/com/berniesanders/canvass/screens/TemplateScreen.java
@@ -6,79 +6,165 @@ import com.berniesanders.canvass.FTBApplication;
 import com.berniesanders.canvass.R;
 import com.berniesanders.canvass.annotations.Layout;
 import com.berniesanders.canvass.dagger.FtbScreenScope;
-import com.berniesanders.canvass.events.ChangePageEvent;
+import com.berniesanders.canvass.dagger.MainComponent;
 import com.berniesanders.canvass.mortar.FlowPathBase;
-import com.berniesanders.canvass.views.AddAddressView;
 import com.berniesanders.canvass.views.TemplateView;
 
 import javax.inject.Inject;
 
+import dagger.Provides;
 import mortar.ViewPresenter;
 import timber.log.Timber;
 
 /**
+ * Example for creating new Mortar Screen that helps explain how it all works
  *
+ * Set the @Layout annotation to the resource id of the layout for the screen
  */
-@Layout(R.layout.screen_add_address)
+@Layout(R.layout.screen_template)
 public class TemplateScreen extends FlowPathBase {
 
+    private final String someData;
 
-    public TemplateScreen() {
+    /**
+     * Constructor called by Flow throughout the app
+     *
+     * Example:
+     * Flow.get(context).set(new TemplateScreen("Some Data To Pass");
+     */
+    public TemplateScreen(String someData) {
+        this.someData = someData;
     }
 
+    /**
+     * Create the component defined as an inner class below.
+     *
+     * This component will inject the presenter on the view, and dependencies/module on the presenter.
+     *
+     * You can pass data (someData) from the Screen to its Presenter through this component.
+     *
+     * Remember you must run the gradle 'build' class for Dagger to generate to component code
+     */
     @Override
     public Object createComponent() {
         return DaggerTemplateScreen_Component
                 .builder()
+                .mainComponent(FTBApplication.getComponent()) //must set if module has (dependencies = MainComponent.class)
+                .templateModule(new TemplateModule(someData)) //pass data to the presenter here
                 .build();
     }
 
+    /**
+     * Important: make this unique to the data you want cages or displayed.
+     * If you show "someData" then add "someData.hashCode()" to the end of this method
+     * This will define the uniqueness that mortar and flow will see this screen for comparisons
+     */
     @Override
     public String getScopeName() {
-        return TemplateScreen.class.getName();// TODO temp scope name?
+        // note someData.hashCode() makes the screen unique
+        return TemplateScreen.class.getName() + someData.hashCode();
     }
 
 
-//    @Module
-//    class Module {
-//
-//    }
+    @dagger.Module
+    class TemplateModule {
 
+        private final String someDataToInject;
+
+        /**
+         * pass variables to the component that will then be injected to the presenter
+         */
+        public TemplateModule(String someDataToInject) {
+            this.someDataToInject = someDataToInject;
+        }
+
+        @Provides
+        String provideSomeData() {
+            return someDataToInject;
+        }
+    }
+
+    /**
+     * This component is used to inject the view with the presenter once the view is inflated.
+     *
+     * The view will injected itself using this component on inflate.
+     *
+     * Expose anything you want injected to the presenter here
+     *
+     * Only use "dependencies = MainComponent.class" if you need something from the main component
+     * Only use "modules = TemplateModule.class" if you need a module
+     */
     @FtbScreenScope
-    @dagger.Component
+    @dagger.Component(modules = TemplateModule.class, dependencies = MainComponent.class)
     public interface Component {
+        /**
+         * injection target = the view (TemplateView) to have the presented injected on it
+         */
         void inject(TemplateView t);
+
+        // Expose anything you want injected to the presenter here
+        // such as Gson from the MainComponent
+
+        // Gson exposeGson();
     }
 
     @FtbScreenScope
     static public class Presenter extends ViewPresenter<TemplateView> {
 
+        /**
+         * Since the presenter is static it should survive rotation
+         */
+        private final String someInjectedData;
 
+        /**
+         * When the view is inflated, this presented is automatically injected to the TemplateView
+         * Constructor parameters here are injected automatically
+         */
         @Inject
-        Presenter() {
+        Presenter(String someInjectedData) {
+            this.someInjectedData = someInjectedData;
         }
 
+        /**
+         * called when the presenter and view are ready.
+         * getView() will not be null
+         *
+         * @param savedInstanceState  This bundle is only passed on rotation not passed on navigating back
+         */
         @Override
         protected void onLoad(Bundle savedInstanceState) {
             Timber.v("onLoad");
-            new ChangePageEvent()
-                    .with(FTBApplication.getEventBus())
-                    .close(true)
-                    .hideToolbar(true)
-                    .dispatch();
+
+            // Some example Page events.
+
+//            new ChangePageEvent()
+//                    .with(FTBApplication.getEventBus())
+//                    .close(true)
+//                    .hideToolbar(true)
+//                    .dispatch();
 
 //            new ShowToolbarEvent()
 //                    .with(FTBApplication.getEventBus())
 //                    .showToolbar(true)
 //                    .dispatch();
-
-
         }
 
+        /**
+         * called on rotation only
+         */
         @Override
         protected void onSave(Bundle outState) {
         }
 
+
+        /**
+         * last chance at the view before it is detached
+         *
+         * You can save state with hack, (restore it the same way by reading the field).
+         * objects saved with be "parceled" by gson. Example:
+         *
+         * ((TemplateScreen)Path.get(view.getContext())).somePublicField = "Something you want to save"
+         */
         @Override
         public void dropView(TemplateView view) {
             super.dropView(view);
--- app/src/main/java/com/berniesanders/canvass/views/TemplateView.java
+++ app/src/main/java/com/berniesanders/canvass/views/TemplateView.java
@@ -5,7 +5,6 @@ import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
 import com.berniesanders.canvass.mortar.DaggerService;
-import com.berniesanders.canvass.screens.AddAddressScreen;
 import com.berniesanders.canvass.screens.TemplateScreen;
 
 import javax.inject.Inject;
@@ -13,17 +12,21 @@ import javax.inject.Inject;
 import timber.log.Timber;
 
 /**
- *
+ * Example mortar screen.
+ * Change what it extends as needed. Any View/Layout type is fine to extend
  */
 public class TemplateView extends FrameLayout {
 
+    /**
+     * Make sure you are pointing at the correct presenter type
+     * YourScreen.Presenter
+     */
     @Inject
     TemplateScreen.Presenter presenter;
 
     public TemplateView(Context context) {
         super(context);
         injectSelf(context);
-
     }
 
     public TemplateView(Context context, AttributeSet attrs) {
@@ -37,7 +40,11 @@ public class TemplateView extends FrameLayout {
     }
 
 
-
+    /**
+     * This is how the presenter is injected on to this view
+     *
+     * Important to note component type is how the DaggerService finds the right component
+     */
     private void injectSelf(Context context) {
         DaggerService.<TemplateScreen.Component>
                 getDaggerComponent(context, DaggerService.DAGGER_SERVICE)

d357385febf71ec789afbd96c9044c8b4bbaefd4
patrickcousins <pj@pj-co.com>
2015-11-21 23:17:02 -0500

    Use LocationController on MapView and listen for CameraChange events

--- app/src/main/java/com/berniesanders/canvass/views/MapScreenView.java
+++ app/src/main/java/com/berniesanders/canvass/views/MapScreenView.java
@@ -1,18 +1,15 @@
 package com.berniesanders.canvass.views;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
 import com.berniesanders.canvass.R;
 import com.berniesanders.canvass.controllers.ActionBarService;
+import com.berniesanders.canvass.controllers.LocationService;
 import com.berniesanders.canvass.mortar.DaggerService;
 import com.berniesanders.canvass.mortar.HandlesBack;
 import com.berniesanders.canvass.screens.AddAddressScreen;
@@ -29,6 +26,7 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
@@ -38,6 +36,11 @@ import butterknife.OnClick;
 import flow.Flow;
 import flow.path.Path;
 import flow.path.PathContext;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 import timber.log.Timber;
 
 /**
@@ -118,8 +121,7 @@ public class MapScreenView extends FrameLayout implements HandlesBack {
                     Timber.v("OnMapReadyCallback");
                     MapScreenView.this.googleMap = gmap;
                     gmap.setMyLocationEnabled(true);
-                    gmap.moveCamera(CameraUpdateFactory
-                            .newCameraPosition(getCurrentLocationCam()));
+                    setCameraPosition(gmap);
                 }
             });
         }
@@ -169,34 +171,88 @@ public class MapScreenView extends FrameLayout implements HandlesBack {
         }
     }
 
+    private void setCameraPosition(final GoogleMap map) {
 
-    //TODO implement actual location code
-    CameraPosition getCurrentLocationCam() {
+        locationObserver = new Observer<Location>() {
+            @Override
+            public void onCompleted() {
+                Timber.v("setCameraPosition onCompleted");
+            }
 
-        LocationManager locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(activityWeakReference.get(),
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(activityWeakReference.get(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            //return null //TODO;
-            Timber.e("location permission error");
-            return null;
+            @Override
+            public void onError(Throwable e) {
+                Timber.e(e, "setCameraPosition onError");
+            }
+
+            @Override
+            public void onNext(Location location) {
+                map.moveCamera(CameraUpdateFactory
+                        .newCameraPosition(getCameraPosition(location)));
+
+                watchCamera(map)
+                        .subscribeOn(AndroidSchedulers.mainThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .debounce(1, TimeUnit.SECONDS)
+                        .subscribe(cameraObserver);
+            }
+        };
+
+        LocationService.get(this)
+                .get()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(locationObserver);
+    }
+
+    Observer<Location> locationObserver;
+
+    Observer<CameraPosition> cameraObserver = new Observer<CameraPosition>() {
+        @Override
+        public void onCompleted() {
+
         }
 
+        @Override
+        public void onError(Throwable e) {
+            Timber.e(e, "onError");
+        }
 
-        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        @Override
+        public void onNext(CameraPosition cameraPosition) {
+            Timber.v("onNext");
+        }
+    };
+
+    private CameraPosition getCameraPosition(Location location) {
         return new CameraPosition
                 .Builder()
                 .target(new LatLng(location.getLatitude(), location.getLongitude()))
                 .zoom(18f)
                 .build();
+    }
 
+
+
+    private Observable<CameraPosition> watchCamera(final GoogleMap map) {
+
+        return Observable.create(new Observable.OnSubscribe<CameraPosition>() {
+
+            @Override
+            public void call(final Subscriber<? super CameraPosition> subscriber) {
+
+                GoogleMap.OnCameraChangeListener camChangeListener =
+                        new GoogleMap.OnCameraChangeListener() {
+
+                    @Override
+                    public void onCameraChange(CameraPosition camPosition) {
+                        Timber.v("onCameraChange");
+                        subscriber.onNext(camPosition);
+                    }
+                };
+
+                map.setOnCameraChangeListener(camChangeListener);
+            }
+        });
     }
 
     @Override

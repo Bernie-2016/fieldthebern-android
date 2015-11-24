0b740ae272f2fa11528708c2c602c1cad96070b3
patrickcousins <pj@pj-co.com>
2015-11-24 12:03:33 -0500

    Remove unneeded double null check for single token null check in TokenRepo

--- app/src/main/java/com/berniesanders/canvass/repositories/TokenRepo.java
+++ app/src/main/java/com/berniesanders/canvass/repositories/TokenRepo.java
@@ -51,7 +51,7 @@ public class TokenRepo {
         //TODO: re-auth based on the type of user?
         Preference<String> tokenPref = rxPrefs.getString(Token.PREF_NAME);
 
-        if (tokenPref==null || tokenPref.get() == null) { return null; }
+        if (tokenPref.get() == null) { return null; }
         return gson.fromJson(tokenPref.get(), Token.class);
     }
 

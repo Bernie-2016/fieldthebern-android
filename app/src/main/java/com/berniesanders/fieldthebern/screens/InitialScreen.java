9169b3f5c34eda8aaeb90271d9643fa110f7a540
patrickcousins <pj@pj-co.com>
2015-11-28 22:42:03 -0500

    re-enable InitialScreen

--- app/src/main/java/com/berniesanders/fieldthebern/screens/InitialScreen.java
+++ app/src/main/java/com/berniesanders/fieldthebern/screens/InitialScreen.java
@@ -1,5 +1,8 @@
 package com.berniesanders.fieldthebern.screens;
 
+import com.berniesanders.fieldthebern.models.Token;
+import com.berniesanders.fieldthebern.models.User;
+import com.f2prateek.rx.preferences.Preference;
 import com.f2prateek.rx.preferences.RxSharedPreferences;
 import com.google.gson.Gson;
 
@@ -23,27 +26,27 @@ public class InitialScreen {
     public Object get() {
 
 //Temp commented out logic until login is completed
-//
-//        Preference<String> userPref = rxPrefs.getString(User.PREF_NAME);
-//
-//        User user = gson.fromJson(userPref.get(), User.class);
-//
-//        if (user != null) { // user has registered previously
-//            Preference<String> tokenPref = rxPrefs.getString(Token.PREF_NAME);
-//            Token token = gson.fromJson(tokenPref.get(), Token.class);
-//
-//            if (token != null) { //user logged in at some point
-//
-//                if (token.isExpired(System.currentTimeMillis())) {
-//                    //TODO try to auth with the token?
-//                    return new LoginScreen(user);
-//                }
-//
-//                return new HomeScreen();
-//            }
-//
-//            return new LoginScreen(user);
-//        }
+
+        Preference<String> userPref = rxPrefs.getString(User.PREF_NAME);
+
+        User user = gson.fromJson(userPref.get(), User.class);
+
+        if (user != null) { // user has registered previously
+            Preference<String> tokenPref = rxPrefs.getString(Token.PREF_NAME);
+            Token token = gson.fromJson(tokenPref.get(), Token.class);
+
+            if (token != null) { //user logged in at some point
+
+                if (token.isExpired(System.currentTimeMillis())) {
+                    //TODO try to auth with the token?
+                    return new LoginScreen(user);
+                }
+
+                return new HomeScreen();
+            }
+
+            return new LoginScreen(user);
+        }
 
         return new ChooseSignupScreen();
     }

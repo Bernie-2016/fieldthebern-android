dc8ca95f6486c78dd050e29e6733e397c5065236
patrickcousins <pj@pj-co.com>
2015-11-29 09:02:25 -0500

    Fix token isExpired and test, created was seconds since epoch, not milliseconds

--- app/src/test/java/com/berniesanders/fieldthebern/models/TokenTest.java
+++ app/src/test/java/com/berniesanders/fieldthebern/models/TokenTest.java
@@ -8,13 +8,15 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class TokenTest {
 
+    public static final int TWO_HOURS_MILLIS = (2 * 1000 * 60 * 60);
+
 
     @Test
     public void testIsExpired_isExpiredForEqualTime() {
         long now = System.currentTimeMillis();
 
         Token token = new Token()
-                .created(now)
+                .created(now/1000)
                 .expires(0);
 
         assertThat(token.isExpired(now)).isTrue();
@@ -25,7 +27,7 @@ public class TokenTest {
         long now = System.currentTimeMillis();
 
         Token token = new Token()
-                .created(now)
+                .created(now/1000)
                 .expires(1);
 
         assertThat(token.isExpired(now+1000)).isTrue();
@@ -36,7 +38,7 @@ public class TokenTest {
         long now = System.currentTimeMillis();
 
         Token token = new Token()
-                .created(now)
+                .created(now/1000)
                 .expires(1000);
 
         assertThat(token.isExpired(now)).isFalse();
@@ -47,7 +49,7 @@ public class TokenTest {
         long now = System.currentTimeMillis();
 
         Token token = new Token()
-                .created(now)
+                .created(now/1000)
                 .expires(-1);
 
         assertThat(token.isExpired(now)).isTrue();
@@ -73,5 +75,36 @@ public class TokenTest {
                 .expires(-2700);
 
         assertThat(token.isExpired(now)).isTrue();
+    }
+    @Test
+    public void testIsExpired_fromSampleApiData() {
+        final long now = 1447745123000L; //ms since epoch
+        final long nowSecondsSinceEpoch = 1447745123;//value from api
+        final int expiresInSeconds = 7200; //2hrs
+
+
+        long twoHoursInTheFuture = now + TWO_HOURS_MILLIS;
+
+        Token token = new Token()
+                .created(nowSecondsSinceEpoch)
+                .expires(expiresInSeconds);
+
+        assertThat(token.isExpired(twoHoursInTheFuture)).isTrue();
+    }
+
+    @Test
+    public void testIsExpired_validFor1hr59mins() {
+        final long now = 1447745123000L;
+        final long nowSecondsSinceEpoch = 1447745123;//value from api
+        final int expiresInSeconds = 7200; //2hrs
+
+
+        long twoHoursInTheFuture = now + TWO_HOURS_MILLIS-2;
+
+        Token token = new Token()
+                .created(nowSecondsSinceEpoch)
+                .expires(expiresInSeconds);
+
+        assertThat(token.isExpired(twoHoursInTheFuture)).isFalse();
     }
 }

f549e4af3fec3710b0fea043a7eaf4cf392732de
patrickcousins <pj@pj-co.com>
2015-12-11 00:57:35 -0500

    Party icon hack

--- app/src/main/java/com/berniesanders/fieldthebern/media/PartyIcon.java
+++ app/src/main/java/com/berniesanders/fieldthebern/media/PartyIcon.java
@@ -5,6 +5,8 @@ import android.support.annotation.DrawableRes;
 import com.berniesanders.fieldthebern.R;
 import com.berniesanders.fieldthebern.models.Party;
 
+import timber.log.Timber;
+
 /*
  * Made with love by volunteers
  * Copyright 2015 BernieSanders.com, FeelTheBern.org, 
@@ -19,19 +21,26 @@ public class PartyIcon {
 
         switch (party) {
             case Party.DEMOCRAT:
+            case Party.DEMOCRAT_AFFILIATION :
                 return R.drawable.ic_democrat;
             case Party.INDEPENDENT:
+            case Party.INDEPENDENT_AFFILIATION :
                 return R.drawable.ic_independent;
             case Party.REPUBLICAN:
+            case Party.REPUBLICAN_AFFILIATION :
                 return R.drawable.ic_republican;
             case Party.UNAFFILIATED:
+            case Party.UNAFFILIATED_AFFILIATION :
                 return R.drawable.ic_other;
             case Party.UNDECLARED:
+            case Party.UNDECLARED_AFFILIATION :
                 return R.drawable.ic_undeclared;
             case Party.UNKNOWN:
+            case Party.UNKNOWN_AFFILIATION :
                 return R.drawable.ic_undeclared;
             default:
-                throw new IllegalArgumentException("cant find PartyIcon for "+party);
+                Timber.e("cant find PartyIcon for "+party);
+                return R.drawable.ic_other;
         }
     }
 }

efc836abe493bf941937c7b6778d043b413d425e
patrickcousins <pj@pj-co.com>
2016-01-09 04:02:49 -0500

    Remove unused code

--- app/src/main/java/com/berniesanders/fieldthebern/controllers/PhotoController.java
+++ app/src/main/java/com/berniesanders/fieldthebern/controllers/PhotoController.java
@@ -114,51 +114,6 @@ public class PhotoController extends Presenter<PhotoController.Activity> {
         }
     }
 
-
-//
-//    private void setPic() {
-////        // Get the dimensions of the View
-////        int targetW = mImageView.getWidth();
-////        int targetH = mImageView.getHeight();
-////
-////        // Get the dimensions of the bitmap
-////        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-////        bmOptions.inJustDecodeBounds = true;
-////        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-////        int photoW = bmOptions.outWidth;
-////        int photoH = bmOptions.outHeight;
-////
-////        // Determine how much to scale down the image
-////        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-////
-////        // Decode the image file into a Bitmap sized to fill the View
-////        bmOptions.inJustDecodeBounds = false;
-////        bmOptions.inSampleSize = scaleFactor;
-////        bmOptions.inPurgeable = true;
-////
-////        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-////        mImageView.setImageBitmap(bitmap);
-//    }
-//
-//    String mCurrentPhotoPath;
-//
-//    private File createImageFile() throws IOException {
-//        // Create an image file name
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        String imageFileName = "JPEG_" + timeStamp + "_";
-//        File storageDir = Environment.getExternalStoragePublicDirectory(
-//                Environment.DIRECTORY_PICTURES);
-//        File image = File.createTempFile(
-//                imageFileName,  /* prefix */
-//                ".jpg",         /* suffix */
-//                storageDir      /* directory */
-//        );
-//
-//        // Save a file: path for use with ACTION_VIEW intents
-//        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-//        return image;
-//    }
-
     /**
      * required by mortar
      */

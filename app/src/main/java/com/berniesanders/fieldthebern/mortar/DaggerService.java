d7fe34672aa50a4408eeced4bac79dc4c2306dcc
patrickcousins <pj@pj-co.com>
2016-01-18 04:25:51 -0500

    Add Apache 2 license to all source files

--- app/src/main/java/com/berniesanders/fieldthebern/mortar/DaggerService.java
+++ app/src/main/java/com/berniesanders/fieldthebern/mortar/DaggerService.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Copyright 2015 FeelTheBern.org
  */
 package com.berniesanders.fieldthebern.mortar;
 
--- app/src/main/java/com/berniesanders/fieldthebern/views/MainView.java
+++ app/src/main/java/com/berniesanders/fieldthebern/views/MainView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Square Inc.
+ * Copyright (c) 2016 - Bernie 2016, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Copyright 2015 FeelTheBern.org
  */
 
 package com.berniesanders.fieldthebern.views;
